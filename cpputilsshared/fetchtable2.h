f8b8e92e4ea1c837d6bcfc661e303ed7a8a1f715
wamas <kingleo@gmx.at>
2009-11-06 12:21:03 +0000

    Weiterentwicklung

--- cpputilsshared/fetchtable2.h
+++ cpputilsshared/fetchtable2.h
@@ -29,60 +29,115 @@ namespace Tools {
 #  define WORKAROUND_BUG_20809_TExecSqlV TExecSqlV
 #endif
 
-template <class Table> class FetchTable : public JVector<Table>
+class FetchTableBase
 {
  protected:
   void *tid;
   SqlContext *sc;
-  std::string table_name;
   std::string where;
   const int BLOCKSIZE;
-
   bool failed;
-
   std::string sql;
 
+ protected:
+  FetchTableBase()
+	: 
+	tid(0),
+	sc(0),
+	where(""),
+	BLOCKSIZE(100),
+	failed(false),
+	sql("")
+	{
+
+	}
+
+  FetchTableBase( void *tid_, SqlContext *sc_, const std::string where_ , const unsigned int BLOCKSIZE_ )
+	:
+	tid(tid_), 
+	sc(sc_), 
+	where( where_ ),
+	BLOCKSIZE( BLOCKSIZE_ ),
+    failed(false),
+	sql( "" )
+	{}
+
+  FetchTableBase( const FetchTableBase & table )
+	:
+	tid(table.tid),
+	sc(table.sc),
+	where(table.where),
+	BLOCKSIZE(table.BLOCKSIZE),
+	failed(table.failed),
+	sql(table.sql)
+	{}
+
+
+  void check_bug_20809()
+	{
+	  if( std::string( __FILE__ ).find(":") != std::string::npos &&
+		  std::string( TSqlErrTxt(tid) ).find( "too few bind variables" ) != std::string::npos )  
+		{
+		  throw REPORT_EXCEPTION( Tools::format("WARNING YOU TRIGGERT BUG http://bugzilla.salomon.at/show_bug.cgi?id=20809\n"
+												"Possible Workaround: recompiling your file with defined FIX_BUG_20809 before\n"
+												"including this '" __FILE__ "' Header file.\n" 
+												"SqlError: %d SqlError is: %s", TSqlError, TSqlErrTxt(tid) )
+								  );
+		}
+	}
+
+  FetchTableBase & operator=( const FetchTableBase & table )
+	{
+	  tid = table.tid;
+	  sc = table.sc;
+	  where = table.where;
+	  failed = table.failed;
+	  sql = table.sql;
+
+	  return *this;
+	}
+
+public:
+    bool operator!() const { return failed; }
+
+	const std::string & get_sql() const { return sql; }
+
+};
+
+template <class Table> class FetchTable : public JVector<Table>, public FetchTableBase
+{
+ protected:
+  std::string table_name;
+
  private:
-  FetchTable() : BLOCKSIZE(0) {};
+  FetchTable() : FetchTableBase(), table_name("") {};
 
  public:
   FetchTable(const FetchTable<Table> & table )
 	: JVector<Table>(table.BLOCKSIZE),
-	  tid(table.tid),
-	  table_name(table.table_name),
-	  where(table.where),
-	  BLOCKSIZE(table.BLOCKSIZE),
-	  failed(table.failed),
-	  sql(table.sql)
+	FetchTableBase( table ),
+	table_name(table.table_name)
 	{
 		insert( table.begin(), table.end() );
 	}
 
   FetchTable & operator=( const FetchTable<Table> & table )
 	{
+	  FetchTableBase::operator=( table );
+
 	  JVector<Table>::clear();
 	  JVector<Table>::insert( JVector<Table>::end(), table.begin(), table.end() );	
-	  tid = table.tid;
-	  sc = table.sc;
-	  table_name = table.table_name;
-	  where = table.where;
-	  failed = table.failed;
-	  sql = table.sql;
-	  
+	  table_name = table.table_name;	  
+
 	  return *this;
 	}
 
  public:
   FetchTable( void *tid_, SqlContext *sc_, const std::string & table_name_, const std::string where_ = "", const unsigned int BLOCKSIZE_ = 100 )
     : JVector<Table>(BLOCKSIZE_),
-    tid(tid_), 
-	sc(sc_), 
-	table_name( table_name_ ), 
-	where( where_ ), 
-	BLOCKSIZE( BLOCKSIZE_ ),
-    failed(false),
-	sql("")
-      {
+	FetchTableBase( tid_, sc_, where_, BLOCKSIZE_ ),
+	table_name( table_name_ )
+	{
 		sql = Tools::format( "select %%%s from %s %s", table_name, table_name, where );
 		exec();
       }
@@ -113,16 +168,7 @@ template <class Table> class FetchTable : public JVector<Table>
 			{
 			  failed = true;
 
-			  if( std::string( __FILE__ ).find(":") != std::string::npos &&
-				  std::string( TSqlErrTxt(tid) ).find( "too few bind variables" ) != std::string::npos )  
-				{
-				  throw REPORT_EXCEPTION( Tools::format("WARNING YOU TRIGGERT BUG http://bugzilla.salomon.at/show_bug.cgi?id=20809\n"
-										  "Possible Workaround: recompiling your file with defined FIX_BUG_20809 before\n"
-										  "including this '" __FILE__ "' Header file.\n" 
-										  "SqlError: %d SqlError is: %s", TSqlError, TSqlErrTxt(tid) )
-										  );
-				}
-
+			  check_bug_20809();
 			  return;
 			}
 		  
@@ -134,54 +180,35 @@ template <class Table> class FetchTable : public JVector<Table>
 
  public:
     Table & operator[]( unsigned int i ) { return JVector<Table>::operator[](i); }
-    
-    bool operator!() const { return failed; }
-
-	const std::string & get_sql() const { return sql; }
 };
 
 
-template <class TableA, class TableB> class FetchTablePair : public JVector<std::pair<TableA,TableB> >
+template <class TableA, class TableB> class FetchTablePair : public JVector<std::pair<TableA,TableB> >, public FetchTableBase
 {
  protected:
-  void *tid;
-  SqlContext *sc;
   std::string table1_name;
   std::string table2_name;
-  std::string where;
-  const int BLOCKSIZE;
-
-  bool failed;
-
-  std::string sql;
 
  private:
-  FetchTablePair() : BLOCKSIZE(0) {};
+  FetchTablePair() : FetchTableBase(), table1_name(""), table2_name("") {};
 
  public:
   FetchTablePair(const FetchTablePair<TableA,TableB> & table )
 	: JVector<std::pair<TableA,TableB> >(),
-	  tid(table.tid),
-	  table1_name(table.table1_name),
-	  table2_name(table.table2_name),
-	  where(table.where),
-	  BLOCKSIZE(table.BLOCKSIZE),
-	  failed(table.failed),
-	  sql(table.sql)
+	FetchTableBase( table ),
+	table1_name(table.table1_name),
+	table2_name(table.table2_name)
 	{
 		insert( table.begin(), table.end() );
 	}
 
   FetchTablePair & operator=( const FetchTablePair<TableA, TableB> & table )
 	{
-		insert( table.begin(), table.end() );	
-		tid = table.tid;
-		sc = table.sc;
-		table1_name = table.table1_name;
-		table2_name = table.table2_name;
-		where = table.where;
-		failed = table.failed;
-		sql = table.sql;
+	  FetchTableBase::operator=( table );
+	  
+	  insert( table.begin(), table.end() );	
+	  table1_name = table.table1_name;
+	  table2_name = table.table2_name;
 	}
 
  public:
@@ -192,14 +219,9 @@ template <class TableA, class TableB> class FetchTablePair : public JVector<std:
 			  const std::string & where_, 
 			  const unsigned int BLOCKSIZE_ = 100 )
     : JVector<std::pair<TableA,TableB> >(),
-    tid(tid_), 
-	sc(sc_), 
+	FetchTableBase(tid_, sc_, where_, BLOCKSIZE_ ),
 	table1_name( table1_name_ ), 
-	table2_name( table2_name_ ), 
-	where( where_ ), 
-	BLOCKSIZE( BLOCKSIZE_ ),
-    failed(false),
-	sql("")
+	table2_name( table2_name_ )
       {
 		sql = Tools::format( "select %%%s, %%%s from %s, %s %s", table1_name, table2_name, table1_name, table2_name, where );
 		exec();
@@ -220,18 +242,20 @@ template <class TableA, class TableB> class FetchTablePair : public JVector<std:
 	  
       do
 		{
-		  rv = ( rv == 0 ) ?  TExecSqlX( tid, sc, 
+		  rv = ( rv == 0 ) ? WORKAROUND_BUG_20809_TExecSqlX( tid, sc, 
 										 const_cast<char*>(sql.c_str()),
 										 BLOCKSIZE, 0,
 										 SELSTRUCT( const_cast<char*>(table1_name.c_str()), vec1[0] ),
 										 SELSTRUCT( const_cast<char*>(table2_name.c_str()), vec2[0] ),
 										 NULL )
-			: TExecSqlV( tid, sc, NULL, NULL, NULL, NULL );
+			:  WORKAROUND_BUG_20809_TExecSqlV( tid, sc, NULL, NULL, NULL, NULL );
 		  
 		  
 		  if( rv < BLOCKSIZE  && TSqlError(tid) != SqlNotFound )	    
 			{
 			  failed = true;
+
+			  check_bug_20809();
 			  return;
 			}
 		  
@@ -249,9 +273,6 @@ template <class TableA, class TableB> class FetchTablePair : public JVector<std:
  public:
     std::pair<TableA,TableB> & operator[]( unsigned int i ) { return JVector<std::pair<TableA,TableB> >::operator[](i); }
     
-    bool operator!() const { return failed; }
-
-	const std::string & get_sql() const { return sql; }
 };
 
 #endif
