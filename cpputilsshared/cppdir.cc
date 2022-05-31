bfe137800c658745fb53f26b46084394fdafd2eb
Martin Oberzalek <kingleo@gmx.at>
2022-05-31 14:40:18 +0200

    Don't exclude cppdir on Windows
    Change-Id: I13baed6c3d813ea6f036a8084fbdf80a4d6935c9

--- cpputilsshared/cppdir.cc
+++ cpputilsshared/cppdir.cc
@@ -1,8 +1,3 @@
-
-#if (defined _WIN32 || defined WIN32)
-// Windows native  : Exclude file
-#else
-
 #include "cppdir.h"
 
 #undef OUT
@@ -17,6 +12,7 @@ extern "C" {
 
 }
 
+#include <sys/types.h>
 #include <vector>
 #include <cstdlib>
 
@@ -284,19 +280,34 @@ CppDir::EFILE CppDir::File::get_type( const std::string& cname )
 
 #undef IS
 
+#if _MSC_VER+0 <= 1900 && !defined( S_ISREG ) // Visual Studio 2015
+# define S_ISDIR( x ) ( x & _S_IFDIR )
+# define S_ISCHR( x ) ( x & _S_IFCHR )
+# define S_ISFIFO( x ) ( x & _S_IFIFO )
+# define S_ISREG( x ) ( x & _S_IFREG )
+#endif
+
+#ifdef S_ISREG
  if( S_ISREG( stat_buf.st_mode ) )
     return EFILE::REGULAR;
+#endif	
 
-
+#ifdef S_ISDIR
   if( S_ISDIR( stat_buf.st_mode ) )
     return EFILE::DIR;
+#endif	
     
+#ifdef S_ISCHR
   if( S_ISCHR( stat_buf.st_mode ) )
     return EFILE::CHAR;
-#ifndef WIN32
+#endif	
+
+#ifdef S_ISBLK
   if( S_ISBLK( stat_buf.st_mode ) )
     return EFILE::BLOCK;
+#endif
 
+#ifdef S_ISFIFO
   if( S_ISFIFO( stat_buf.st_mode ) )
     return EFILE::FIFO;
 #endif	
@@ -806,5 +817,3 @@ bool CppDir::is_in_dir( const std::string &path, const std::string &dir )
 
   return false;
 }
-
-#endif // WIN32 && _MSC_VER_
